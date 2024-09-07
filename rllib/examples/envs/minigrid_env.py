a9df63a879750db1d8e779da6b3e1adaeb1d5814
sven1977 <svenmika1977@gmail.com>
2024-09-07 21:20:47 +0200

    learns >0.6 on multi-room N4 in < 400 iterations
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/examples/envs/minigrid_env.py
+++ rllib/examples/envs/minigrid_env.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
             train_batch_size_per_learner=2000,
             lr=0.0003,
             vf_loss_coeff=10.0,
-            entropy_coeff=0.01,
+            entropy_coeff=0.01,#
             learner_config_dict={
                 # Intrinsic reward coefficient.
                 # Tune this parameter such that the term:
