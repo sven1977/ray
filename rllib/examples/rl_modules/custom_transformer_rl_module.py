b8c906b02fbd41f343cca091a73a527b65ce9d07
sven1977 <svenmika1977@gmail.com>
2024-09-13 16:23:56 +0200

    seems to only get to ~0.5 reward (randomly picking any object at the intersection). Could have to do with the fact that agent is not even looking (at the beginning while still in the large room) at the object to go to ...
    Signed-off-by: sven1977 <svenmika1977@gmail.com>

--- rllib/examples/rl_modules/custom_transformer_rl_module.py
+++ rllib/examples/rl_modules/custom_transformer_rl_module.py
@@ -85,10 +85,10 @@ if __name__ == "__main__":
         )
         .training(
         #    learner_connector=lambda in_o, in_a: AddTransformerInputToBatchLearner(),
-            lr=0.00015,#[[0, 0.00005], [300000, 0.00015]],
+            lr=[[0, 0.00005], [200000, 0.0001]],
             sgd_minibatch_size=256,
             num_sgd_iter=6,
-            vf_loss_coeff=0.1,
+            vf_loss_coeff=1.0,
             entropy_coeff=0.005,
             #grad_clip=1.0,
         )
